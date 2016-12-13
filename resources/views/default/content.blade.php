51834d4d91852dd3526a5e03e44202b89c81f204
Viktor <meitslin@gmail.com>
2016-12-13 20:13:35 +0200

    third

--- resources/views/default/content.blade.php
+++ resources/views/default/content.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
 	<div class="col-sm-8 blog-main">
-		@if(isset($articles) && $articles->count() > 0)		            
+		@if(isset($articles) && $articles && $articles->count() > 0)		            
 			@foreach($articles as $article)
 				<div class="blog-post">
 					<h2 class="blog-post-title">
